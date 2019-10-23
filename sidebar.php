9beedfda1a6a02a17d5c6bceb9c0c2c3274f654a
hiroshi_yoshida <u.dance.gm@gmail.com>
2019-10-24 02:03:03 +0900

    step 3-1

--- sidebar.php
+++ sidebar.php
@@ -1,8 +1,8 @@
-	  <section id="sidebar">
-		<div id="primary" class="widget-area">
-			subnavi
-		</div><!-- #primary end -->
-		<div id="id" class="widget-area">
-			facebook like box
+<section id="sidebar">
+	<div id="primary" class="widget-area">
+		<?php dynamic_sidebar( 'primary-widget-area' ); ?>
+</div><!-- #primary end -->
+<div id="id" class="widget-area">
+	facebook like box
 		</div>
-	  </section><!-- #sidebar end -->
+</section><!-- #sidebar end -->
